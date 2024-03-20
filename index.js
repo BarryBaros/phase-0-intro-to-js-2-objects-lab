567fdfe6be61b4aee7a223891100f20cb193bc19
Timothy Barasa <timbaros87@gmail.com>
2024-03-20 20:20:26 +0300

    Add objects

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+
+let employee = {
+    name: "Tim Baros",
+    streetAddress: "Mombasa Road"
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
+
+console.log(updateEmployeeWithKeyAndValue(employee, "position", "Manager"));
