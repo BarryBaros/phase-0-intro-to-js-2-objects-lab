4fbbaa0e641080e15d60ac6cdd040c0ff99199c6
Timothy Barasa <timbaros87@gmail.com>
2024-03-31 12:00:11 +0300

    creat, add, modify and delete objects

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return {
-        ...employee,
-        [key]: value
-    };
-}
-
-let employee = {
-    name: "Tim Baros",
-    streetAddress: "Mombasa Road"
+// Assigning an employee variable to an object using literal syntax
+const employee = {
+  name: "Tim Baros",
+  streetAddress: "1230 Mombasa Road",
+  age: "25",
+  position: "Developer"
 };
 
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+      ...employee,
+      [key]: value
+  };
+}
+const updatedEmployee = updateEmployeeWithKeyAndValue(employee, 'streetAddress', '456 Manyanja Road');
+
+
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-    return employee;
+  employee[key] = value;
+  return employee;
 }
 
 function deleteFromEmployeeByKey(employee, key) {
-    const newEmployee = { ...employee };
-    delete newEmployee[key];
-    return newEmployee;
+  const newEmployee = { ...employee }; // Create a copy of the employee object
+  delete newEmployee[key]; // Delete the property with the specified key from the copy
+  return newEmployee; // Return the modified copy
 }
+// const deletedvalue = deleteFromEmployeeByKey(employee, 'age');
+delete employee.age;
+
+
 
 
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key];
-    return employee;
+  delete employee[key];
+  return employee;
 }
+console.log(updateEmployeeWithKeyAndValue(employee));
 
-
-console.log(updateEmployeeWithKeyAndValue(employee, "position", "Manager"));
